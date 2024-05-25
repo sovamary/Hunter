68af97aa5216c0e02652101ae80b17e6da98e943
sovamary <ITmarypetrova@gmail.com>
2024-05-26 00:08:42 +0300

    Initial commit

---
+++ Assets/Scripts/GunControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+
+    private float horizontal;
+    private float vertical;
+    private float sensitivity = 2;
+
+
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    void Update()
+    {
+        horizontal = Input.GetAxis("Mouse X") * sensitivity;
+        vertical = Input.GetAxis("Mouse Y") * sensitivity;
+        transform.Rotate(0, horizontal, 0);
+        transform.Rotate(0,0,vertical);
+
+    }
+}
